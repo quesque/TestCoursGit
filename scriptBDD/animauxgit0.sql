89edd571e26a921ebaf7437ad2419c9eb0a3ce02
Séverine Quesque <severinequesque@gmail.com>
2019-08-22 18:14:25 +0200

    création table type et clé étrangère dans animaux

--- scriptBDD/animauxgit0.sql
+++ scriptBDD/animauxgit0.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 22 août 2019 à 15:37
+-- Généré le :  jeu. 22 août 2019 à 15:26
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.0.23
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `animauxgit0`
+-- Base de données :  `animauxgit`
 --
 
 -- --------------------------------------------------------
@@ -34,22 +34,54 @@ CREATE TABLE IF NOT EXISTS `animal` (
   `nom` varchar(30) NOT NULL,
   `age` int(11) NOT NULL,
   `sexe` tinyint(1) NOT NULL,
+  `idType` int(11) NOT NULL,
   `image` varchar(50) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  PRIMARY KEY (`idAnimal`)
+  PRIMARY KEY (`idAnimal`),
+  KEY `FK_TYPE` (`idType`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `animal`
 --
 
-INSERT INTO `animal` (`idAnimal`, `nom`, `age`, `sexe`, `image`, `type`) VALUES
-(1, 'Tya', 7, 0, 'Tya/Tya.jpg', 'chat'),
-(2, 'Milo', 3, 1, 'Milo/Milo.jpg', 'chat'),
-(3, 'Mina', 2, 0, 'Mina/Mina.jpg', 'chien'),
-(4, 'Felicia', 4, 0, 'Felicia/Felicia.jpg', 'chat'),
-(5, 'Lili', 1, 0, 'Lili/Lili.jpg', 'chat'),
-(6, 'Hoquet', 7, 1, 'Hoquet/Hoquet.jpg', 'chien');
+INSERT INTO `animal` (`idAnimal`, `nom`, `age`, `sexe`, `idType`, `image`) VALUES
+(1, 'Tya', 7, 0, 2, 'Tya/Tya.jpg'),
+(2, 'Milo', 3, 1, 2, 'Milo/Milo.jpg'),
+(3, 'Mina', 2, 0, 1, 'Mina/Mina.jpg'),
+(4, 'Felicia', 4, 0, 2, 'Felicia/Felicia.jpg'),
+(5, 'Lili', 1, 0, 2, 'Lili/Lili.jpg'),
+(6, 'Hoquet', 7, 1, 1, 'Hoquet/Hoquet.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE IF NOT EXISTS `type` (
+  `idType` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(30) NOT NULL,
+  PRIMARY KEY (`idType`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `type`
+--
+
+INSERT INTO `type` (`idType`, `libelle`) VALUES
+(1, 'chien'),
+(2, 'chat');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `animal`
+--
+ALTER TABLE `animal`
+  ADD CONSTRAINT `FK_TYPE` FOREIGN KEY (`idType`) REFERENCES `type` (`idType`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
